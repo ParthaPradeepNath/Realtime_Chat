d781343ebe5b088c84fb9e7304aca2db399504b2
ParthaPradeepNath <parthanath96134@gmail.com>
2025-12-18 19:44:43 +0530

    client modify

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,13 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
+// remove trailing slash if present
+const PUBLIC_API = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, '')
 
-export const client = treaty<App>('localhost:3000').api
+// in the browser use the current origin (deployed app) unless NEXT_PUBLIC_API_URL is set
+const apiUrl =
+  typeof window !== 'undefined'
+    ? PUBLIC_API || window.location.origin
+    : PUBLIC_API || 'http://localhost:3000'
+
+export const client = treaty<App>(apiUrl).api
